0d6ac20cf017395b2f8ab44bd712228ce463d9a7
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2023-05-01 13:08:26 -0300

    update

--- posintall.sh
+++ posintall.sh
@@ -45,19 +45,6 @@ else
 	echo -e "${RED}${software} NÃO instalado${NC}"
 fi
 
-# Instalação timeshift
-software='TimeShift'
-echo -e "Deseja instalar ${RED}[${software}]${NC}? s/y: "
-read option
-if [ $option == "Y" ] || [ $option == "y" ] || [ $option == "S" ] || [ $option == "s" ]
-then
-	echo -e "${RED}Instalando ${software}${NC}"
-	sudo apt install timeshift -y
-	echo -e "${RED}${software} instalado${NC}"
-else
-	echo -e "${RED}${software} NÃO instalado${NC}"
-fi
-
 # Instalação hollywood
 software='Hollywood'
 echo -e "Deseja instalar ${RED}[${software}]${NC}? s/y: "
