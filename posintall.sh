073d0205d2cf9fd1335ae8d4e3598ab7553441c4
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2023-05-06 17:54:10 -0300

    Update
    add cpu-x

--- posintall.sh
+++ posintall.sh
@@ -45,6 +45,20 @@ else
 	echo -e "${RED}${software} NÃO instalado${NC}"
 fi
 
+# Instalação cpu-x
+software='CPU-X'
+echo -e "Deseja instalar ${RED}[${software}]${NC}? s/y: "
+read option
+if [ $option == "Y" ] || [ $option == "y" ] || [ $option == "S" ] || [ $option == "s" ]
+then
+	echo -e "${RED}Instalando ${software}${NC}"
+	sudo apt install cpu-x -y
+	echo -e "${RED}${software} instalado${NC}"
+else
+	echo -e "${RED}${software} NÃO instalado${NC}"
+fi
+# ---------------------------------------------------------------------- #
+
 # Instalação hollywood
 software='Hollywood'
 echo -e "Deseja instalar ${RED}[${software}]${NC}? s/y: "
